bb85bdd2c51254f793e0c8e378d54e62164dc6f1
Aayush shah <58617484+ios-shah@users.noreply.github.com>
2025-05-20 20:31:07 +0530

    Update whatsapp-web.js

--- whatsapp-web.js
+++ whatsapp-web.js
@@ -73,9 +73,10 @@ async function start() {
       }
     });
 
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
+});
+
 
   } catch (error) {
     console.error('Failed to start application:', error);
